c9f27b00f52be2d14093ab4b9a0b169afd5e53aa
Andrej Vojtuš <avojtus@gmail.com>
2019-10-27 01:32:15 +0200

    Added ytsearch library

--- ytmp3.py
+++ ytmp3.py
@@ -5,7 +5,7 @@ import inspect
 from os import path
 from threading import Thread
 from youtube_dl import YoutubeDL
-from youtube_api import YoutubeDataApi
+from ytsearch import ytsearch
 
 class ytmp3(object):
     options = {
@@ -19,39 +19,20 @@ class ytmp3(object):
         'ffmpeg_location': path.join(path.dirname(path.abspath(inspect.stack()[0][1])), "ffmpeg")
     }
 
-    def __init__(self, api_key):
+    def __init__(self):
         self.log = logging.getLogger("ytmp3")
         ytmp3.options["logger"] = self.log
-        self.api_key = api_key
-        self.api = YoutubeDataApi(api_key)
+        self.yts = ytsearch()
         self.ytd = YoutubeDL(ytmp3.options)
 
     def _load_file(self, filename):
         pass
 
-    def search(self, query, max_results = 6):
+    def search(self, query):
         if not isinstance(query, str):
             raise Exception("query must be a string type")
 
-        results = []
-        try:
-            for v in self.api.search(query, max_results=max_results):
-                vid = {}
-                vid['title'] = v['video_title']
-                vid['author'] = v['channel_title']
-                vid['id'] = v["video_id"]
-                vid['url'] = v["video_id"]
-                vid['date'] = v['video_publish_date']
-                vid['thumb'] = v['video_thumbnail']
-                results.append(vid)
-
-                self.log.debug(f"Title: {v['video_title']}, Url: {v['video_id']}, Date: {v['video_publish_date']}, \
-                Author:{v['channel_title']}, Thumb: {v['video_thumbnail']}")
-
-        except Exception as ex:
-            self.log.error(f"Error while scraping youtube: {ex}")
-
-        return results
+        return self.yts.search(query)
 
     def download(self, video_ids):
         if not isinstance(next(iter(video_ids), None), str):
@@ -59,22 +40,22 @@ class ytmp3(object):
         
         self.ytd.download(video_ids)
 
-    def search_and_download(self, query, callback, max_results = 6):
+    def search_and_download(self, query, callback):
         if not callable(callback):
             raise Exception("callback must be a callable object")
         
-        results = self.search(query, max_results)
+        results = self.search(query)
 
         index = 0
         if len(results) > 0:
             index = callback(results)
 
         if index >= 0 and index < len(results):
-            self.log.info(f"Downloading '{results[index].get('title')}'")
+            self.log.info(f"Downloading '{results[index].get('title')}'...")
             self.download([results[index].get('id')])
+            self.log.info(f"Done!")
         else:
-            raise Exception("callback returned invalid index")
-        self.log.info(f"Done")
+            raise Exception("callback returned invalid index") 
         
     def read_and_download(self, filename, delimeter = '\n'):
         if not isinstance(filename, str) or not isinstance(delimeter, str):
@@ -85,7 +66,8 @@ if __name__ == "__main__":
     logging.basicConfig(format="[%(filename)s:%(lineno)d]:%(levelname)s: %(message)s", level=logging.INFO)
     yt = ytmp3()
     
-    inp = ""
+    logging.info("Please input the name(s) of song(s) to download")
+    inp = input()
     while inp != "exit":
-        inp = input("Search query: ")
-        Thread(target = yt.search_and_download, args = (inp, lambda res: 0, 1)).start()
+        Thread(target = yt.search_and_download, args = (inp, lambda res: 0)).start()
+        inp = input()
